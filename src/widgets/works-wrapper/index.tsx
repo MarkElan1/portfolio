70b0ddea63b226c2abd504ff457edcc6d6d9dfb2
Mark Elan <killorden@gmail.com>
2024-01-22 21:40:48 +0200

    bugFix

--- src/widgets/works-wrapper/index.tsx
+++ src/widgets/works-wrapper/index.tsx
@@ -3,7 +3,7 @@ import './index.scss';
 const WorksWrapper = () => {
   return (
     <div className="WorksWrapper">
-      <div className="workds-head">
+      <div className="works-head">
         <h2 className="title">My Works</h2>
         <span className="sub-title">Some of my latest projects</span>
       </div>
