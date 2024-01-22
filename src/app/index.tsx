78843e3e4ceb61c25b2698eccc85aebdfe4574ce
Mark Elan <killorden@gmail.com>
2024-01-22 20:32:36 +0200

    Add Home page and connected it with store to App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
+import Home from 'pages/home';
 import './index.scss'
+import { useDarkTheme } from 'shared/lib/useDarkTheme';
 
 function App() {
+  const darkTheme = useDarkTheme(state => state.darkTheme);
   return (
-    <div className='App'>
-      work
+    <div className={`App ${darkTheme ? 'dark' : ''}`}>
+      <Home/>
     </div>
   )
 }
