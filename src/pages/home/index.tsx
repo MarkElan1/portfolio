4cb14154a713e34566177c54522449698f9d93d6
Mark Elan <killorden@gmail.com>
2024-01-22 20:59:05 +0200

    Add new widget Works Wrapper

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 import Intro from "widgets/intro";
+import WorksWrapper from "widgets/works-wrapper";
 
 const Home = () => {
   return (
     <div className="Home">
       <Intro/>
+      <WorksWrapper/>
     </div>
   )
 }
---
+++ src/widgets/works-wrapper/index.tsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+const WorksWrapper = () => {
+  return (
+    <div className="WorksWrapper">
+      <div className="workds-head">
+        <h2 className="title">My Works</h2>
+        <span className="sub-title">Some of my latest projects</span>
+      </div>
+    </div>
+  )
+}
+
+export default WorksWrapper;
