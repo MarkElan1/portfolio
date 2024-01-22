8de17d83bf37bbcc679abe2989936f12c22486dc
Mark Elan <killorden@gmail.com>
2024-01-22 20:36:21 +0200

    Refactored button

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -5,11 +5,12 @@ interface ButtonProps {
   href: string;
   target?: string;
   children: React.ReactNode;
+  className: string;
 }
 
-const Button: React.FC<ButtonProps> = ({children, href, target}) => {
+const Button: React.FC<ButtonProps> = ({children, href, target, className}) => {
   return (
-    <a href={href} target={target} className='button'>
+    <a href={href} target={target} className={`button ${className}`}>
       {children}
     </a>
   )
