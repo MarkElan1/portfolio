fd23ff243e9a503c1bb8d6d4dd0969bf6e317a12
Mark Elan <killorden@gmail.com>
2024-01-22 22:22:21 +0200

    Add Not Supported for mobile breakpoint

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,16 @@ import { useDarkTheme } from 'shared/lib/useDarkTheme';
 
 function App() {
   const darkTheme = useDarkTheme(state => state.darkTheme);
+
+  if (window.innerWidth <= 900) {
+    return (
+      <div className={`NotSupported ${darkTheme ? 'dark' : ''}`}>
+        <h1 className='title'>Not supported on mobile</h1>
+        <span className='sub-title'>Coming Soon</span>
+      </div>
+    )
+  }
+
   return (
     <div className={`App ${darkTheme ? 'dark' : ''}`}>
       <Home/>
